d7e7c44ee2e818a52f575cc6e0438ed2e7b59ea5
Jinjie Meng <875302177@qq.com>
2018-06-10 12:52:16 +0800

    ver1.1
    修复无法执行python文件的错误。

--- bwb_lottery_everyday.sh
+++ bwb_lottery_everyday.sh
@@ -5,8 +5,8 @@ datasuffix='txt'                                #datasuffix
 
 a=`date -d "${date}" +%w`
 if [ $a -eq 1 ] || [ $a -eq 3 ] || [ $a -eq 7 ]; then
-    python grab500_ssq.py $datapath $datasuffix
+    python "${basepath}/grab500_ssq.py" $datapath $datasuffix
 elif [ $a -eq 2 ] || [ $a -eq 4 ] || [ $a -eq 6 ]; then
-    python grab500_dlt.py $datapath $datasuffix
+    python "${basepath}/grab500_dlt.py" $datapath $datasuffix
 fi
 
