6c439bb57eccdd42230a5bac526b330c10c7efed
Jinjie Meng <875302177@qq.com>
2018-06-10 13:06:52 +0800

    1.2.1
    修复星期天的时间

--- bwb_lottery_everyday.sh
+++ bwb_lottery_everyday.sh
@@ -4,7 +4,7 @@ datapath=$basepath'/lotterydata'                #shell's datadir
 datasuffix='txt'                                #datasuffix
 
 a=`date -d "${date}" +%w`
-if [ $a -eq 1 ] || [ $a -eq 3 ] || [ $a -eq 7 ]; then
+if [ $a -eq 1 ] || [ $a -eq 3 ] || [ $a -eq 0 ]; then
     python "${basepath}/grab500_ssq.py" $datapath $datasuffix
 elif [ $a -eq 2 ] || [ $a -eq 4 ] || [ $a -eq 6 ]; then
     python "${basepath}/grab500_dlt.py" $datapath $datasuffix
