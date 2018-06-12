c8214b78d9919a600a7c771ee7f631f60e7d7673
Jinjie Meng <875302177@qq.com>
2018-06-12 18:18:01 +0800

    v2.2
    修复爬取日期的错误

--- bwb_lottery_everyday.sh
+++ bwb_lottery_everyday.sh
@@ -4,9 +4,9 @@ datapath=$basepath'/lotterydata'                #shell's datadir
 datasuffix='txt'                                #datasuffix
 
 a=`date -d "${date}" +%w`
-if [ $a -eq 1 ] || [ $a -eq 3 ] || [ $a -eq 0 ]; then
+if [ $a -eq 1 ] || [ $a -eq 3 ] || [ $a -eq 6 ]; then
     python "${basepath}/grab500_ssq.py" $datapath $datasuffix
-elif [ $a -eq 2 ] || [ $a -eq 4 ] || [ $a -eq 6 ]; then
+elif [ $a -eq 2 ] || [ $a -eq 4 ] || [ $a -eq 0 ]; then
     python "${basepath}/grab500_dlt.py" $datapath $datasuffix
 fi
 
